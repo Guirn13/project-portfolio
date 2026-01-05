782f3f1f9fe1104aae7a956705edf7fbffe70fd6
Guilherme <guinevers10@gmail.com>
2026-01-04 22:20:29 -0300

    feat: add DotGrid component and integrate GSAP for animations

--- src/components/Homes/index.jsx
+++ src/components/Homes/index.jsx
@@ -1,13 +1,36 @@
-import Typewriter from "../Typewriter"
+import DotGrid from "../DotGrid";
+import Typewriter from "../Typewriter";
 
 export default function Homes() {
-    return (
-        <section id="home" className="h-180 flex items-center justify-center bg-gradient-to-r from-sky-600 to-sky-900 text-white text-center">
-            <div>
-                
-                <Typewriter className="text-5xl font-bold mb-4" text="Bem-vindo ao meu portfólio" speed={150} />
-                <p className="text-2xl">I am developer🧑🏻‍💻</p>
-            </div>
-        </section>
-    )
+  return (
+    <section
+      id="home"
+      className="h-180 relative overflow-hidden flex items-center bg-gray-900 justify-center"
+    >
+      <div className="absolute inset-0 z-0">
+        <DotGrid
+          dotSize={6}
+          gap={15}
+          baseColor="#00A6F4"
+          activeColor="#ffffff"
+          proximity={120}
+          speedTrigger={100}
+          shockRadius={250}
+          shockStrength={5}
+          maxSpeed={5000}
+          resistance={750}
+          returnDuration={1.5}
+        />
+      </div>
+
+      <div className="relative z-10 text-center text-gray-100">
+        <Typewriter
+          className="text-5xl font-bold mb-4 "
+          text="Bem-vindo ao meu portfólio"
+          speed={150}
+        />
+        <p className="text-2xl">I am developer 🧑🏻‍💻</p>
+      </div>
+    </section>
+  );
 }
